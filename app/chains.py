827124c6c1bfc68ee57f80211c68b17af132e544
Biswas Simkhada <biswas.simk@bissi-smells.modem>
2024-09-05 11:26:03 +1000

    answer question functionality

--- app/chains.py
+++ app/chains.py
@@ -1,5 +1,4 @@
 import os
-import PyPDF2
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
@@ -40,41 +39,58 @@ class Chain:
             raise OutputParserException("Context too big. Unable to parse jobs.")
         return json_res if isinstance(json_res, list) else [json_res]
 
-    def write_email(self, job, links):
-        
-        with open('app/resources/resume.pdf', 'rb') as file:
-            reader = PyPDF2.PdfReader(file)
-
-            for page_num in range(len(reader.pages)):
-                page = reader.pages[page_num]
-                resume_content = page.extract_text()
-        
-        with open('app/resources/email_template.txt', 'r') as file:
-            email_temp = file.read()
-        
+    def write_email(self, job, links, resume, email):
         prompt_email = PromptTemplate.from_template(
             """
-        ### JOB DESCRIPTION:
-        {job_description}
-        
-        ### RESUME CONTENT:
-        {resume_content}
-        
-        ### INSTRUCTION:
-        You are the person whose resume is attached above. Your skills, experiences and education match theirs exactly
-        Your job is to write a cold email to a job recruiter regarding the job mentioned above in the position of the person the resume is about, showcasing your expertise in the skills required by the job description, your ability to build scalable and efficient solutions, and your experience with relevant technologies that meet their needs.
-        Write this cold email in the format of the email template below
-        Also, ONLY add the most relevant project from the following links to highlight your portfolio: {link_list}
-        
-        ### EMAIL TEMPLATE:
-        {email_temp}
-        
-        Do not provide a preamble.
-        ### EMAIL (NO PREAMBLE):
-        """
+            ### JOB DESCRIPTION:
+            {job_description}
+            ### RESUME CONTENT:
+            {resume_content}
+            ### INSTRUCTION:
+            You are the person whose resume is attached above. Your skills, experiences and education match theirs exactly
+            Your job is to write a cold email to a job recruiter regarding the job mentioned above in the position of the person the resume is about, showcasing your expertise in the skills required by the job description, your ability to build scalable and efficient solutions, and your experience with relevant technologies that meet their needs.
+            Write this cold email in the format of the email template below
+            Also, ONLY add the most relevant project from the following links relevant to the job description: {link_list}
+            ### EMAIL TEMPLATE:
+            {email_temp}
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+            """
         )
 
         chain_email = prompt_email | self.llm
-        res = chain_email.invoke({"job_description": str(job),"resume_content": resume_content, "link_list": links, "email_temp": email_temp})
+        res = chain_email.invoke({
+            "job_description": str(job),
+            "resume_content": resume, 
+            "link_list": links, 
+            "email_temp": email
+        })
+        return res.content
+
+
+    def write_answer(self, job, resume, question):
+        
+        prompt_question = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+            ### RESUME CONTENT:
+            {resume_content}
+            ### INSTRUCTION:
+            You are the person whose resume is attached above. Your skills, experiences, and education match theirs exactly.
+            Your job is to answer the question below from the perspective of the person the resume is about, using your expertise, skills, and experiences as relevant to the question.
+            Write your answer in a detailed, concise, and professional manner, drawing from the resume content provided.
+            ### QUESTION:
+            {question}
+            Do not provide a preamble.
+            ### ANSWER (NO PREAMBLE):
+            """
+        )
+
+        chain_email = prompt_question | self.llm
+        res = chain_email.invoke({
+            "job_description": str(job),
+            "resume_content": resume, 
+            "question": question})
         return res.content
 
--- app/main.py
+++ app/main.py
@@ -1,15 +1,30 @@
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
+import PyPDF2
 
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
 
+def get_resume():
+    with open('app/resources/resume.pdf', 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        resume_content = ""
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            resume_content += page.extract_text()
+    return resume_content
 
-def create_streamlit_app(llm, portfolio, clean_text):
-    st.title("Intern Apply AI")
-    url_input = st.text_input("Enter a URL:", value="https://example.com/job/posting-number")
-    submit_button = st.button("Submit")
+def get_email_temp():
+    with open('app/resources/email_template.txt', 'r') as file:
+        email_temp = file.read()
+    return email_temp
+
+def generate_email_page(llm, portfolio, clean_text):
+    st.title("Generate Emails for Job Applications")
+    
+    url_input = st.text_input("Enter a Job Posting URL:", value="https://example.com/job/posting-number")
+    submit_button = st.button("Generate Email")
 
     if submit_button:
         try:
@@ -17,16 +32,83 @@ def create_streamlit_app(llm, portfolio, clean_text):
             data = clean_text(loader.load().pop().page_content)
             portfolio.load_portfolio()
             jobs = llm.extract_jobs(data)
+            resume = get_resume()
+            email = get_email_temp()
             for job in jobs:
                 skills = job.get('skills', [])
                 links = portfolio.query_links(skills)
-                email = llm.write_email(job, links)
+                email = llm.write_email(job, links, resume, email)
                 st.code(email, language='markdown')
         except Exception as e:
             st.error(f"An Error Occurred: {e}")
 
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+import PyPDF2
 
-chain = Chain()
+from chains import Chain
+from portfolio import Portfolio
+from utils import clean_text
+
+def get_resume():
+    with open('app/resources/resume.pdf', 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        resume_content = ""
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            resume_content += page.extract_text()
+    return resume_content
+
+def get_email_temp():
+    with open('app/resources/email_template.txt', 'r') as file:
+        email_temp = file.read()
+    return email_temp
+
+def generate_email_page(llm, portfolio, clean_text):
+    st.title("Generate Emails for Job Applications")
+    url_input = st.text_input("Enter a Job Posting URL:", value="https://example.com/job/posting-number")
+    submit_button = st.button("Generate Email")
+
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            jobs = llm.extract_jobs(data)
+            resume = get_resume()
+            email = get_email_temp()
+            for job in jobs:
+                skills = job.get('skills', [])
+                links = portfolio.query_links(skills)
+                email = llm.write_email(job, links, resume, email)
+                st.code(email, language='markdown')
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+def answer_questions_page(llm, portfolio, clean_text):
+    st.subheader("Ask Application Questions")
+    url_input = st.text_input("Enter a Job Posting URL:", value="https://example.com/job/posting-number")
+    question = st.text_input("Enter your question:")
+    submit_button = st.button("Answer Question")
+    
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            jobs = llm.extract_jobs(data)
+            resume = get_resume()
+            for job in jobs:
+                answer = llm.write_answer(job, resume, question)
+                st.code(answer, language='markdown')
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+
+st.set_page_config(layout="wide", page_title="Intern Apply AI", page_icon="👨🏽‍🎓")
+page = st.sidebar.radio("Select a Page", ("Generate Emails", "Answer Questions"))
+llm = Chain()
 portfolio = Portfolio()
-st.set_page_config(layout="wide", page_title="Intern Apply AI", page_icon="📧")
-create_streamlit_app(chain, portfolio, clean_text)
+portfolio.load_portfolio()
+if page == "Generate Emails":
+    generate_email_page(llm, portfolio, clean_text)
+elif page == "Answer Questions":
+    answer_questions_page(llm, portfolio, clean_text)
